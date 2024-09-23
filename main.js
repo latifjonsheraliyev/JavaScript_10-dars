72ebea0d5295a7f48a50d07f2a092d3f67526ce1
latif <latifjonsheraliyev209@gmail.com>
2024-09-23 19:36:21 +0500

    js

---
+++ main.js
@@ -0,0 +1,46 @@
+// 1_masala
+// function sc(floor) {
+//   return floor > 6 ? "Aa~ ".repeat(floor - 1) + "Pa!" : "Aa~ ".repeat(floor - 1) + "Pa! Aa!";
+// }
+
+// console.log(sc(2)); 
+// console.log(sc(7)); 
+
+
+
+
+
+
+
+// 2-masala 
+// function countTrailingZeros(n) {
+//   if (n === 0) return 0;
+//   let count = 0;
+
+
+//   while ((n & 1) === 0) {
+//     count++;
+//     n >>= 1; 
+//   }
+
+//   return count;
+// }
+
+
+// console.log(countTrailingZeros(4));
+// console.log(countTrailingZeros(5)); 
+
+
+
+
+
+
+
+
+// 3-masala
+// function hero(bullets, dragons) {
+//   return bullets >= dragons * 2;
+// }
+
+// console.log(hero(10, 5));
+// console.log(hero(7, 4));  
